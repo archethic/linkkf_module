fcb1166a581dd988234728807b12336d9f68070c
BlueRex <65700898+RealBlueRex@users.noreply.github.com>
2021-03-12 21:04:43 +0900

    모듈 수정합니다?

--- linkkf.js
+++ linkkf.js
@@ -1,33 +1,31 @@
-module.exports = function () {
-    const Jsoup = org.jsoup.Jsoup;
-    function getTitle(title, grade) {
+importClass(org.jsoup.Jsoup);
+"use strict"
+const linkkf = {
+    getTitle: function (title, grade) {
         try {
             if(grade == undefined || grade == "" || grade == null) grade = 0;
             return JSON.parse(Jsoup.connect("https://linkkf.app/wp-json/wp/v2/posts?search=" + title).ignoreContentType(true).get().text())[grade].title.rendered;
         } catch (e) {
             return "존재하지 않거나 검색하는데 오류가 발생했습니다?"
         }
-    }
-
-    function getLink(title, grade) {
+    },
+    getLink: function (title, grade) {
         try {
             if(grade == undefined || grade == "" || grade == null) grade = 0;
             return JSON.parse(Jsoup.connect("https://linkkf.app/wp-json/wp/v2/posts?search=" + title).ignoreContentType(true).get().text())[grade].link;
         } catch (e) {
             return "존재하지 않거나 검색하는데 오류가 발생했습니다?"
         }
-    }
-
-    function getId(title, grade) {
+    },
+    getId: function (title, grade) {
         try {
             if(grade == undefined || grade == "" || grade == null) grade = 0;
             return JSON.parse(Jsoup.connect("https://linkkf.app/wp-json/wp/v2/posts?search=" + title).ignoreContentType(true).get().text())[grade].id;
         } catch (e) {
             return "존재하지 않거나 검색하는데 오류가 발생했습니다?"
         }
-    }
-
-    function getUploadTime(title, grade) {
+    },
+    getUploadTime: function (title, grade) {
         try {
             if(grade == undefined || grade == "" || grade == null) grade = 0;
             return JSON.parse(Jsoup.connect("https://linkkf.app/wp-json/wp/v2/posts?search=" + title).ignoreContentType(true).get().text())[grade].date;
@@ -35,11 +33,6 @@ module.exports = function () {
             return "존재하지 않거나 검색하는데 오류가 발생했습니다?"
         }
     }
+};
 
-    return {
-        getTitle: getTitle.bind(),
-        getLink: getLink.bind(),
-        getId: getId.bind(),
-        getUploadTime: getUploadTime.bind()
-    }
-}()
+module.exports = linkkf;
